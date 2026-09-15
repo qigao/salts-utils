63cc95ab5a8bf86094471d90d0e7719a09a63fb9
Qi Gao <qi.gao2012@gmail.com>
2026-09-15 19:47:09 +0800

    fix(databind): hide schema fingerprint symbol

--- tbe/data_bind/data_bind_schema_internal.h
+++ tbe/data_bind/data_bind_schema_internal.h
@@ -11,9 +11,17 @@ extern "C" {
 
 #define DATA_BIND_SCHEMA_FINGERPRINT_SIZE 32U
 
-int data_bind_schema_fingerprint(
+#if !defined(_WIN32) && (defined(__GNUC__) || defined(__clang__))
+#define DATA_BIND_SCHEMA_INTERNAL __attribute__((visibility("hidden")))
+#else
+#define DATA_BIND_SCHEMA_INTERNAL
+#endif
+
+DATA_BIND_SCHEMA_INTERNAL int data_bind_schema_fingerprint(
     const Node *root,
     uint8_t out[DATA_BIND_SCHEMA_FINGERPRINT_SIZE]);
+
+#undef DATA_BIND_SCHEMA_INTERNAL
 
 #ifdef __cplusplus
 }
