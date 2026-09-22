96bf052378486ae00f5e94c65c5ec38966b74ef5
Qi Gao <qi.gao2012@gmail.com>
2026-09-22 13:43:31 +0800

    build: use BUILD_TESTS as the only test switch

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -2,9 +2,6 @@ include(CMakeDependentOption)
 
 set(CMAKE_COLOR_DIAGNOSTICS ON)
 
-# building the tests
-option(ENABLE_TESTS "Enable the tests" ON)
-
 # Address Sanitizer - only enabled for Debug builds
 cmake_dependent_option(ENABLE_ASAN "Enable Address Sanitizer" ON
                        "CMAKE_BUILD_TYPE STREQUAL Debug" OFF)
