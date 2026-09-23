820123969c4236658e6ad23d970ac4af25f438ad
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 16:30:26 +0800

    test(databind): compile generic BindingPlan public ABI under C++17

--- databind/runtime/test_data_bind_cmeta_public_cpp.cpp
+++ databind/runtime/test_data_bind_cmeta_public_cpp.cpp
@@ -1,1 +1,9 @@
+#include "data_bind_binding_plan.h"
 #include "test_data_bind_cmeta_public.c"
+
+static_assert(std::is_standard_layout_v<DataBindBindingProjectionSlot>);
+static_assert(std::is_standard_layout_v<DataBindBindingProjection>);
+static_assert(std::is_standard_layout_v<DataBindNativeRecordBinding>);
+static_assert(std::is_standard_layout_v<DataBindServiceNativeBinding>);
+static_assert(std::is_standard_layout_v<DataBindBindingPlanEntry>);
+static_assert(std::is_standard_layout_v<DataBindBindingProvider>);
