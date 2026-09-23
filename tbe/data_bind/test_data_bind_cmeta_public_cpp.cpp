9517bc1218f8ff0ffd9ad444cf9264a3593500fe
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 14:31:29 +0800

    test(databind): compile service plan public ABI under C++17

--- tbe/data_bind/test_data_bind_cmeta_public_cpp.cpp
+++ tbe/data_bind/test_data_bind_cmeta_public_cpp.cpp
@@ -1,1 +1,10 @@
+#include <type_traits>
+
+#include "data_bind_service_plan.h"
 #include "test_data_bind_cmeta_public.c"
+
+static_assert(std::is_standard_layout_v<DataBindServicePlanEntry>);
+static_assert(std::is_standard_layout_v<DataBindServiceNativeBinding>);
+static_assert(std::is_standard_layout_v<DataBindServicePlanDiagnostic>);
+static_assert(std::is_standard_layout_v<DataBindServiceCallFrame>);
+static_assert(std::is_standard_layout_v<DataBindServiceProvider>);
