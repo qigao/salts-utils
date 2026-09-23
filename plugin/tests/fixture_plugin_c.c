0d1d0bb94ae300d3c2efde218262cfde16b4e431
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 08:32:02 +0800

    test(plugin): add C loader fixture

---
+++ plugin/tests/fixture_plugin_c.c
@@ -0,0 +1,14 @@
+#include <salts/plugin.h>
+
+static const salts_plugin_manifest fixture_manifest = {
+    .struct_size = SALTS_PLUGIN_MANIFEST_V1_SIZE,
+    .abi_version = SALTS_PLUGIN_ABI_VERSION,
+    .plugin_id = "test.loader.c",
+    .version = {1u, 0u, 0u},
+};
+
+SALTS_PLUGIN_QUERY_EXPORT
+const salts_plugin_manifest *SALTS_PLUGIN_CALL
+salts_plugin_query(uint32_t host_abi) {
+    return host_abi == SALTS_PLUGIN_ABI_VERSION ? &fixture_manifest : NULL;
+}
