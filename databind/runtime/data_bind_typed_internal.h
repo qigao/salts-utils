3bb9a8708d340d88bc8ae8dd1d1fca96bf1fbb6a
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 21:27:16 +0800

    refactor(databind): rename generic typed runtime away from TBE ownership

---
+++ databind/runtime/data_bind_typed_internal.h
@@ -0,0 +1,15 @@
+#ifndef DATA_BIND_TYPED_INTERNAL_H
+#define DATA_BIND_TYPED_INTERNAL_H
+
+#include "data_bind_typed.h"
+#include <cmeta/data.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DATA_BIND_TYPED_INTERNAL_H */
