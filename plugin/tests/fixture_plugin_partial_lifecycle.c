829b38b70b55faafd649e8a4e50d1517cce59560
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 22:42:57 +0800

    test(plugin): move fixtures to ABI 2

--- plugin/tests/fixture_plugin_partial_lifecycle.c
+++ plugin/tests/fixture_plugin_partial_lifecycle.c
@@ -9,10 +9,10 @@ fixture_start(void *self) {
 }
 
 static const salts_plugin_manifest fixture_manifest = {
-    .struct_size = SALTS_PLUGIN_MANIFEST_V1_SIZE,
+    .struct_size = SALTS_PLUGIN_MANIFEST_SIZE,
     .abi_version = SALTS_PLUGIN_ABI_VERSION,
     .plugin_id = "test.lifecycle.partial",
-    .version = {1u, 0u, 0u},
+    .version = {2u, 0u, 0u},
     .self = &fixture_state,
     .start = fixture_start,
 };
