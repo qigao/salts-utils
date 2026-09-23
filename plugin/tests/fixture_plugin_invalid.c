205c556c0ecec613d711decec190705ad6bfe197
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 22:42:54 +0800

    test(plugin): move fixtures to ABI 2

--- plugin/tests/fixture_plugin_invalid.c
+++ plugin/tests/fixture_plugin_invalid.c
@@ -1,10 +1,10 @@
 #include <salts/plugin.h>
 
 static const salts_plugin_manifest fixture_manifest = {
-    .struct_size = SALTS_PLUGIN_MANIFEST_V1_SIZE,
+    .struct_size = SALTS_PLUGIN_MANIFEST_SIZE,
     .abi_version = SALTS_PLUGIN_ABI_VERSION + 1u,
     .plugin_id = "test.loader.invalid",
-    .version = {1u, 0u, 0u},
+    .version = {2u, 0u, 0u},
 };
 
 SALTS_PLUGIN_QUERY_EXPORT
