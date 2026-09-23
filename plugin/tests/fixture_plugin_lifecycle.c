fc36f2a5102e503ad99e51a6ec3e5b56f4783348
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 09:46:59 +0800

    test(plugin): make lifecycle fixture a complete contract

--- plugin/tests/fixture_plugin_lifecycle.c
+++ plugin/tests/fixture_plugin_lifecycle.c
@@ -5,8 +5,27 @@ static int fixture_state;
 static salts_plugin_status SALTS_PLUGIN_CALL
 fixture_start(void *self) {
     int *state = (int *)self;
-    ++*state;
+    *state = 1;
     return SALTS_PLUGIN_OK;
+}
+
+static salts_plugin_status SALTS_PLUGIN_CALL
+fixture_request_stop(void *self) {
+    int *state = (int *)self;
+    *state = 2;
+    return SALTS_PLUGIN_OK;
+}
+
+static bool SALTS_PLUGIN_CALL
+fixture_is_quiescent(const void *self) {
+    const int *state = (const int *)self;
+    return *state == 2;
+}
+
+static void SALTS_PLUGIN_CALL
+fixture_destroy(void *self) {
+    int *state = (int *)self;
+    *state = 3;
 }
 
 static const salts_plugin_manifest fixture_manifest = {
@@ -16,6 +35,9 @@ static const salts_plugin_manifest fixture_manifest = {
     .version = {1u, 0u, 0u},
     .self = &fixture_state,
     .start = fixture_start,
+    .request_stop = fixture_request_stop,
+    .is_quiescent = fixture_is_quiescent,
+    .destroy = fixture_destroy,
 };
 
 SALTS_PLUGIN_QUERY_EXPORT
