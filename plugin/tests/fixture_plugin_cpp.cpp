8c12199201bbe43b0a8e2710ea33b300195e963f
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 08:32:04 +0800

    test(plugin): add C++ loader fixture

---
+++ plugin/tests/fixture_plugin_cpp.cpp
@@ -0,0 +1,22 @@
+#include <salts/plugin.h>
+
+static const salts_plugin_manifest fixture_manifest = {
+    SALTS_PLUGIN_MANIFEST_V1_SIZE,
+    SALTS_PLUGIN_ABI_VERSION,
+    "test.loader.cpp",
+    {1u, 0u, 0u},
+    0u,
+    nullptr,
+    0u,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr
+};
+
+SALTS_PLUGIN_QUERY_EXPORT
+const salts_plugin_manifest *SALTS_PLUGIN_CALL
+salts_plugin_query(uint32_t host_abi) {
+    return host_abi == SALTS_PLUGIN_ABI_VERSION ? &fixture_manifest : nullptr;
+}
