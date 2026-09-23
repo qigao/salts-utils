60b7e9ab0cd45eda89d9fb1b45bbea9ef320cd11
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 17:59:32 +0800

    chore: remove useless cmake/ci

--- tests/runtime_path/runtime_consumer.c
+++
@@ -1,6 +0,0 @@
-int runtime_path_dependency_value(void);
-int runtime_path_extra_dependency_value(void);
-
-int main(void) {
-  return runtime_path_dependency_value() + runtime_path_extra_dependency_value() == 49 ? 0 : 1;
-}
