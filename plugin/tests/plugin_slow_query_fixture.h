3ff4987d53f6924f048311f3849c68768a03f6ea
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 23:44:26 +0800

    test(plugin): define slow-query synchronization markers

---
+++ plugin/tests/plugin_slow_query_fixture.h
@@ -0,0 +1,9 @@
+#ifndef SALTS_PLUGIN_SLOW_QUERY_FIXTURE_H
+#define SALTS_PLUGIN_SLOW_QUERY_FIXTURE_H
+
+#define PLUGIN_SLOW_QUERY_ENTERED_MARKER \
+    "salts_plugin_slow_query_entered.marker"
+#define PLUGIN_SLOW_QUERY_RELEASE_MARKER \
+    "salts_plugin_slow_query_release.marker"
+
+#endif /* SALTS_PLUGIN_SLOW_QUERY_FIXTURE_H */
