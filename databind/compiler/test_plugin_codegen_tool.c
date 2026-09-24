1a260ea34aaba6d6c3205459a99e0bb5741c4bda
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 10:12:53 +0800

    test(databindc): select ImageProcessor Component for Plugin codegen

--- databind/compiler/test_plugin_codegen_tool.c
+++ databind/compiler/test_plugin_codegen_tool.c
@@ -27,6 +27,7 @@ int main(int argc, char **argv) {
       .plugin_version_major = 1u,
       .plugin_version_minor = 0u,
       .plugin_version_patch = 0u,
+      .component_name = "ImageProcessor",
       .native_header = argv[4],
       .service_header_output = argv[3],
   };
