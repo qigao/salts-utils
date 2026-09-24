3a58c70f7a496f71eb9724d69df9f6b7125a1025
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 18:14:46 +0800

    feat(databind): build generated typed Plugin client target

--- cmake/DataBind.cmake
+++ cmake/DataBind.cmake
@@ -168,6 +168,7 @@ function(databind_target)
   foreach(reserved_target IN ITEMS
           "${DB_TARGET}"
           "${DB_TARGET}_plugin"
+          "${DB_TARGET}_plugin_client"
           "${DB_TARGET}_databind_codegen")
     if(TARGET "${reserved_target}")
       message(FATAL_ERROR
@@ -263,6 +264,10 @@ function(databind_target)
       "${_generated_dir}/${DB_ARTIFACT_NAME}.plugin.h")
   set(_plugin_source
       "${_generated_dir}/${DB_ARTIFACT_NAME}.plugin.c")
+  set(_plugin_client_header
+      "${_generated_dir}/${DB_ARTIFACT_NAME}.plugin_client.h")
+  set(_plugin_client_source
+      "${_generated_dir}/${DB_ARTIFACT_NAME}.plugin_client.c")
 
   set(_generate_dependencies "${_idl}")
   if(_databindc_dependency)
@@ -274,6 +279,8 @@ function(databind_target)
       "${_native_header}"
       "${_plugin_header}"
       "${_plugin_source}"
+      "${_plugin_client_header}"
+      "${_plugin_client_source}"
     COMMAND "${CMAKE_COMMAND}" -E make_directory "${_generated_dir}"
     COMMAND ${_databindc_command}
       "${_idl}"
@@ -292,13 +299,17 @@ function(databind_target)
     "${_native_header}"
     "${_plugin_header}"
     "${_plugin_source}"
+    "${_plugin_client_header}"
+    "${_plugin_client_source}"
     PROPERTIES GENERATED TRUE)
 
   add_custom_target("${DB_TARGET}_databind_codegen"
     DEPENDS
       "${_native_header}"
       "${_plugin_header}"
-      "${_plugin_source}")
+      "${_plugin_source}"
+      "${_plugin_client_header}"
+      "${_plugin_client_source}")
 
   add_library("${DB_TARGET}_plugin" SHARED
     "${_plugin_source}"
@@ -318,8 +329,23 @@ function(databind_target)
     PREFIX ""
     OUTPUT_NAME "${DB_ARTIFACT_NAME}")
 
+  add_library("${DB_TARGET}_plugin_client" STATIC
+    "${_plugin_client_source}"
+    "${_plugin_client_header}"
+    "${_native_header}")
+  add_dependencies("${DB_TARGET}_plugin_client"
+    "${DB_TARGET}_databind_codegen")
+  target_compile_features("${DB_TARGET}_plugin_client" PRIVATE c_std_11)
+  target_include_directories("${DB_TARGET}_plugin_client" PUBLIC
+    "${_generated_dir}")
+  target_link_libraries("${DB_TARGET}_plugin_client" PUBLIC
+    Salts::Plugin
+    Salts::DataBindGeneratedABI)
+
   add_custom_target("${DB_TARGET}")
-  add_dependencies("${DB_TARGET}" "${DB_TARGET}_plugin")
+  add_dependencies("${DB_TARGET}"
+    "${DB_TARGET}_plugin"
+    "${DB_TARGET}_plugin_client")
 
   set_property(TARGET "${DB_TARGET}" PROPERTY
     DATABIND_COMPONENT "${DB_COMPONENT}")
@@ -327,9 +353,13 @@ function(databind_target)
     DATABIND_GENERATED_DIR "${_generated_dir}")
   set_property(TARGET "${DB_TARGET}" PROPERTY
     DATABIND_PLUGIN_TARGET "${DB_TARGET}_plugin")
+  set_property(TARGET "${DB_TARGET}" PROPERTY
+    DATABIND_PLUGIN_CLIENT_TARGET "${DB_TARGET}_plugin_client")
 
   set(${DB_TARGET}_PLUGIN_TARGET
       "${DB_TARGET}_plugin" PARENT_SCOPE)
+  set(${DB_TARGET}_PLUGIN_CLIENT_TARGET
+      "${DB_TARGET}_plugin_client" PARENT_SCOPE)
   set(${DB_TARGET}_GENERATED_DIR
       "${_generated_dir}" PARENT_SCOPE)
 endfunction()
