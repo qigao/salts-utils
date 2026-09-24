2a62aa55305200b1d37fbbac9fba71800b9060d3
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 23:33:07 +0800

    test(databind): compile ValidationPlan public header as C++17

--- databind/runtime/test_data_bind_cmeta_public_cpp.cpp
+++ databind/runtime/test_data_bind_cmeta_public_cpp.cpp
@@ -2,6 +2,7 @@
 
 #include "data_bind_binding_plan.h"
 #include "data_bind_method_plan.h"
+#include "data_bind_validation_plan.h"
 #include "test_data_bind_cmeta_public.c"
 
 static_assert(std::is_standard_layout_v<DataBindSchemaConstraint>);
