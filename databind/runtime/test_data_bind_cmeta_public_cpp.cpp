71b72bf9ca5f0d229e529d34b1ef71f4abedd0b9
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 22:10:37 +0800

    feat(databind): replay HTTP/RPC MethodPlan runtime on BindingPlan outcomes (#201)
    * replay nullable JSON: CMakeLists.txt
    
    * replay nullable JSON: tbe_typed.c
    
    * replay nullable JSON: test_tbe_typed_descriptor_boundary.c
    
    * replay nullable JSON: acceptance test
    
    * feat(databind): add HTTP/RPC MethodPlan runtime contract
    
    * feat(databind): compile HTTP/RPC MethodPlans on BindingPlan outcomes
    
    * build(databind): install MethodPlan runtime contract
    
    * test(databind): map typed Service outcomes through MethodPlans
    
    * test(databind): qualify MethodPlan public C++ layout
    
    * test(databind): reject mismatched transport outcome mapping

--- databind/runtime/test_data_bind_cmeta_public_cpp.cpp
+++ databind/runtime/test_data_bind_cmeta_public_cpp.cpp
@@ -1,6 +1,7 @@
 #include <type_traits>
 
 #include "data_bind_binding_plan.h"
+#include "data_bind_method_plan.h"
 #include "test_data_bind_cmeta_public.c"
 
 static_assert(std::is_standard_layout_v<DataBindBindingAddress>);
@@ -15,3 +16,10 @@ static_assert(std::is_standard_layout_v<DataBindBindingProjection>);
 static_assert(std::is_standard_layout_v<DataBindNativeStateBinding>);
 static_assert(std::is_standard_layout_v<DataBindBindingCallFrame>);
 static_assert(std::is_standard_layout_v<DataBindBindingProvider>);
+
+static_assert(std::is_standard_layout_v<DataBindHttpFieldProjection>);
+static_assert(std::is_standard_layout_v<DataBindHttpErrorMapping>);
+static_assert(std::is_standard_layout_v<DataBindHttpProjectionConfig>);
+static_assert(std::is_standard_layout_v<DataBindRpcFieldProjection>);
+static_assert(std::is_standard_layout_v<DataBindRpcErrorMapping>);
+static_assert(std::is_standard_layout_v<DataBindRpcProjectionConfig>);
