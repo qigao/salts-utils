af03f3a10b7eb3be6dfdcb6efa68059eec648f23
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 18:16:48 +0800

    test(databind): compile installed generated Plugin client in C++17

---
+++ databind/compiler/package_config/databind_target/client_cpp.cpp
@@ -0,0 +1,13 @@
+#include "image.plugin_client.h"
+
+#include <type_traits>
+
+using Client = databind_plugin_client_5_Image_14_ImageProcessor;
+
+static_assert(std::is_standard_layout_v<Client>,
+              "installed generated Plugin client must remain C-compatible");
+
+int main() {
+  Client client{};
+  return client.registry == nullptr ? 0 : 1;
+}
