db35410e180c9ffdbbbd0cc82f1f4de6cda2f2e3
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 18:54:04 +0800

    test(databindc): qualify generated Plugin client INIT macro

--- databind/compiler/test_plugin_generated_client_cpp.cpp
+++ databind/compiler/test_plugin_generated_client_cpp.cpp
@@ -8,6 +8,6 @@ static_assert(std::is_standard_layout_v<Client>,
               "generated Plugin client must remain C-compatible");
 
 int main() {
-  Client client{};
+  Client client = IMAGE_IMAGEPROCESSOR_PLUGIN_CLIENT_INIT;
   return client.registry == nullptr ? 0 : 1;
 }
